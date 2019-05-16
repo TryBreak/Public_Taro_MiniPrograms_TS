99bac1447e038a1a4df6ff40a2a2ad4c2821dd13
Mark <meichangliang@outlook.com>
2019-05-16 13:36:37 +0800

    完成啊~

--- src/pages/demo/pageDemo/index.tsx
+++ src/pages/demo/pageDemo/index.tsx
@@ -3,17 +3,18 @@
  * @Description: In User Settings Edit
  * @Author: Mark
  * @Date: 2019-04-26 11:39:23
-* @LastEditTime: 2019-05-16 13:33:55
+* @LastEditTime: 2019-05-16 13:32:27
  */
 import Taro, { Component } from '@tarojs/taro';
 import { View, Button, Image, Text } from '@tarojs/components';
 import { getUrlParam } from '@/utils/utils';
 import { WebView_domain, baseUrl, image_domain } from '@/config/baseUrl';
+//image
 import { tusiji } from '@/images/load';
+//image
+
 import './index.less';
 
-//image
-//image
 class _page extends Component {
   config = {
     navigationBarTitleText: 'PageDemo',
@@ -25,31 +26,45 @@ class _page extends Component {
     // const token = getStore('userToken');
     this.state = {
       // token,
+      id: '',
     };
   }
 
-  componentWillMount() {
-    console.log(123);
-  }
+  componentWillMount() { }
 
   componentWillReact() { }
 
   componentDidMount() { }
 
-  componentWillUnmount() { }
+  componentWillUnmount() {
+    console.log('离开页面');
+  }
 
   componentDidHide() { }
 
   componentDidShow() {
     let { id } = getUrlParam();
-    console.info('id--', id);
+    if (id) {
+    } else {
+      Taro.showToast({
+        title: '缺少参数,这里将参数设置为默认',
+        icon: 'none',
+      });
+      id = id || '3';
+    }
+    const _this = this;
+    console.log(id);
+    _this.setState({
+      id,
+    });
   }
 
   render() {
+    const { id }: any = this.state;
     return (
       <View className="page">
         这里是demo页面
-        <Button>这里是demo页面 Button</Button>
+        <Button>这里是demo页面 Button {id}</Button>
         <Text className="text">这里是demo页面 Text</Text>
         <Image mode="widthFix" src={tusiji} />
         <View className="base_router">
