4e957f2a9740145e058f7d8993a099f6ca1e3d75
Mark <meichangliang@outlook.com>
2019-05-17 13:20:35 +0800

    update

--- src/app.tsx
+++ src/app.tsx
@@ -28,11 +28,7 @@ class App extends Component {
    * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
    */
   config: Config = {
-    pages: [
-      'pages/index/index',
-      'pages/demo/index',
-      'pages/demo/index',
-    ],
+    pages: ['pages/index/index', 'pages/demo/index',],
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
